abbe5d22707dccc1a96eaacc1f5b7d90183aa97a
Filip Godziński <godzinski.filip@gmail.com>
2018-02-17 00:06:51 +0100

    test - updating records

---
+++ test/update_test.js
@@ -0,0 +1,52 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Updating records', () => {
+    let tomek;
+
+    beforeEach( (done) => {
+        tomek = new User({ name: 'Tomek'});
+        tomek.save()
+            .then( () => done());
+    });
+
+    function assertName(operation, done) {
+        operation
+            .then( () => User.find({}))
+            .then( (users) => {
+                assert(users.length === 1);
+                assert(users[0].name === 'Dawid');
+                done();
+            });
+    }
+
+    it('instance type using set and save', (done) => {
+        tomek.set('name', 'Dawid');
+        assertName(tomek.save(), done);
+    });
+
+    it('A model instance can update', (done) => {
+        assertName(tomek.update({ name: 'Dawid'}), done);
+    });
+
+    it('A model class can update', (done) => {
+        assertName(
+            User.update({ name: 'Tomek' }, { name: 'Dawid' }),
+            done
+        );
+    });
+
+    it('A model class can update one record', (done) => {
+        assertName(
+            User.findOneAndUpdate({ name: 'Tomek' }, { name: 'Dawid' }),
+            done
+        );
+    });
+
+    it('A model class can update find a record with Id and update', (done) => {
+        assertName(
+            User.findByIdAndUpdate(tomek._id, { name: 'Dawid'}),
+            done
+        );
+    });
+});
